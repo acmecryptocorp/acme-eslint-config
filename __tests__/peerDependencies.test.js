f388e10949801255daa8bf4b23bf9f1a8e1e0780
Andrey Mork <35698586+AndreyMork@users.noreply.github.com>
2019-10-03 20:52:30 +0300

    test: update tests

--- __tests__/peerDependencies.test.js
+++ __tests__/peerDependencies.test.js
@@ -1,23 +1,57 @@
 // @flow
 
 import { peerDependencies } from '../package.json';
-import { plugins } from '../src/eslintrc.json';
+import { plugins as flowPlugins, extends as flowConfigs } from '../flow';
 
+import { plugins as generalPlugins, extends as generalConfigs } from '..';
 
-const normalizePluginName = (pluginName: string): string =>
-  (pluginName.startsWith('@') ? pluginName : `eslint-plugin-${pluginName}`);
+const isScoped = (name: string): boolean => name.startsWith('@');
 
-const normalizedPluginNames: $ReadOnlyArray<string> = plugins.map(
-  (pluginName: string): string => normalizePluginName(pluginName),
-);
+const normalizeName = (name: string, type: 'config' | 'plugin'): string =>
+  isScoped(name) ? name : `eslint-${type}-${name}`;
 
-const constantPeerDeps: $ReadOnlyArray<string> = ['eslint'];
-const targetPeerDependencies: Set<string> = new Set([
-  ...constantPeerDeps,
-  ...normalizedPluginNames,
-]);
-const actualPeerDependencies: Set<string> = new Set(Object.keys(peerDependencies));
+const normalizePluginName = (name: string): string =>
+  normalizeName(name, 'plugin');
+const normalizeConfigName = (name: string): string =>
+  normalizeName(name, 'config');
+
+const merge = (
+  arrays: $ReadOnlyArray<?$ReadOnlyArray<string>>,
+): $ReadOnlyArray<string> =>
+  arrays.reduce(
+    (
+      acc: $ReadOnlyArray<string>,
+      items: ?$ReadOnlyArray<string>,
+    ): $ReadOnlyArray<string> => (items == null ? acc : [...acc, ...items]),
+    [],
+  );
+
+const mergeAndNormalize = (
+  arrays: $ReadOnlyArray<?$ReadOnlyArray<string>>,
+  normalizeFunction: (string) => string,
+): $ReadOnlyArray<string> => merge(arrays).map(normalizeFunction);
 
 test('peerDependencies', () => {
+  const plugins: $ReadOnlyArray<string> = mergeAndNormalize(
+    [generalPlugins, flowPlugins],
+    normalizePluginName,
+  );
+
+  const configs: $ReadOnlyArray<string> = mergeAndNormalize(
+    [generalConfigs, flowConfigs],
+    normalizeConfigName,
+  );
+
+  const constantPeerDeps: $ReadOnlyArray<string> = ['eslint'];
+  const targetPeerDependencies: Set<string> = new Set([
+    ...constantPeerDeps,
+    ...plugins,
+    ...configs,
+  ]);
+
+  const actualPeerDependencies: Set<string> = new Set(
+    Object.keys(peerDependencies),
+  );
+
   expect(actualPeerDependencies).toEqual(targetPeerDependencies);
 });
